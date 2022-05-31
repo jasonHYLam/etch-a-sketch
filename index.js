0f64e249bcb24f46670ff7e89cfa28da83bd1039
jaleel <jasonlamdarksouls@gmail.com>
2022-05-31 21:14:54 +0200

    Turn div background black when hovering mouse over

--- index.js
+++ index.js
@@ -13,10 +13,19 @@ for (let i = 1; i <= 16; i++) {
 
     for (let j = 1; j <= 16; j++) {
         const square = document.createElement("div");
-        square.className = "row";
+        square.className = "square";
         square.innerText = j
         row.appendChild(square);
     }
     divContainer.appendChild(row);
 
 }
+
+const allSquares = document.querySelectorAll(".square");
+
+allSquares.forEach(e => {
+    e.addEventListener('mouseover',function(event) {
+        event.target.style.background = "black";
+    })
+});
+
