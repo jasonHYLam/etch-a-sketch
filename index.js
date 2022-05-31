45b5654f962b047820eab6020609769523f30a12
jaleel <jasonlamdarksouls@gmail.com>
2022-05-31 22:25:30 +0200

    Add hover functionality for new grids
    Added hover functionality for grids created by button. Also set square
    height and width using flexbox.

--- index.js
+++ index.js
@@ -1,38 +1,53 @@
-const divContainer = document.querySelector("#divContainer");
+function establishHover() {
+let allSquares = document.querySelectorAll(".square");
 
-createGrid(16)
-function createGrid(gridLength) {
-
-for (let i = 1; i <= gridLength; i++) {
-    const row = document.createElement("div");
-    row.className = "row";
-
-    for (let j = 1; j <= gridLength; j++) {
-        const square = document.createElement("div");
-        square.className = "square";
-        square.innerText = j
-        row.appendChild(square);
-    }
-    divContainer.appendChild(row);
-
-}
-}
-
-const allSquares = document.querySelectorAll(".square");
 
 allSquares.forEach(e => {
     e.addEventListener('mouseover',function(event) {
         event.target.style.background = "black";
     });
 });
+const divContainer = document.querySelector("#divContainer");
+
+}
+
+
+function removeGrid() {
+    while (divContainer.lastChild) {
+        divContainer.lastChild.remove();
+    }
+};
+
+function createGrid(gridLength) {
+    for (let i = 1; i <= gridLength; i++) {
+        const row = document.createElement("div");
+        row.className = "row";
+
+        for (let j = 1; j <= gridLength; j++) {
+            const square = document.createElement("div");
+            square.className = "square";
+            square.style.border = 'solid';
+            // square.innerText= 'hahaiha! ';
+            row.appendChild(square);
+        }
+        divContainer.appendChild(row);
+    }
+}
+
+
+function setGridSize() {
+    let gridSize = prompt("Please set the size of the grid (Max size is 100)");
+    removeGrid();
+    createGrid(gridSize);
+    establishHover();
+
+}
+
+createGrid(16);
+establishHover();
 
 const button = document.querySelector("#setSizeButton");
 
 button.addEventListener('click', function(event) {
     setGridSize();
 });
-
-function setGridSize() {
-    let gridSize = prompt("Please set the size of the grid (Max size is 100)");
-    createGrid(gridSize);
-}
